41341f27495fe12e8a978b9bd38286bf49a9b51c
sfmsakib <skfuadsakib@gmail.com>
2022-07-31 12:45:36 +0600

    changes

--- aldebarandialog/src/main/java/com/sprout/aldebarandialog/AbsAldebaranDialog.kt
+++ aldebarandialog/src/main/java/com/sprout/aldebarandialog/AbsAldebaranDialog.kt
@@ -144,9 +144,6 @@ abstract class AbsAldebaranDialog<Self: AbsAldebaranDialog<Self, T>, T: AbsAldeb
 //                // alpha value may need to be set again after this call
 //                background.color = topColor
 //            }
-
-
-
         }
         return this as T
     }
